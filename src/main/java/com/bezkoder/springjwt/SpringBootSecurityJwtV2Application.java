ccc697e69066ae3c005f9670ded448c3be822cf2
SERINEZAGHDOUD19 <zaserine19@gmail.com>
2022-04-06 14:42:03 +0100

    first commit

---
+++ src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtV2Application.java
@@ -0,0 +1,13 @@
+package com.bezkoder.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwtV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bezkoder/springjwt/SpringBootSecurityJwtV2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bezkoder.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBootSecurityJwtV2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
